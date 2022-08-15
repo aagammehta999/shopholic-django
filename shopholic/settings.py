ace0a643af7f9c3c3d8d55028071c81c0b30857a
aagammehta999 <aagammehta49@gmail.com>
2022-08-15 21:52:51 +0530

    changes

--- shopholic/settings.py
+++ shopholic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['shopholic-env.eba-mhbrqidh.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['shopholic-pro-env.eba-mycp5b8q.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
